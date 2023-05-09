882841740fd9ca3cd6026e6391bddaec526fc4d1
claireconner <clairemconner@gmail.com>
2023-05-08 20:18:33 -0700

    change chatgpt response

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -26,7 +26,8 @@ public class IntegrationTest {
             System.out.println(prompt);
             String response = ChatGPT.getResponse(prompt, 1000); 
             assertEquals(prompt, "Who was the tallest man alive?");
-            assertEquals(response, "The tallest man ever recorded was Robert Wadlow, an American man who stood 8 feet 11.1 inches (2.72 meters) tall. He lived from 1918 to 1940."); 
+            //assertEquals(response, "The tallest man ever recorded was Robert Wadlow, an American man who stood 8 feet 11.1 inches (2.72 meters) tall. He lived from 1918 to 1940."); 
+            assertEquals(response, "At the time of writing, the tallest man alive was Sultan Kösen, a Turkish farmsman who measured 8 feet and 3 inches tall. He held the Guinness World Record for the tallest living man since 2009."); 
         } catch (Exception e) {
             System.out.println("You suck bro"); 
         }
