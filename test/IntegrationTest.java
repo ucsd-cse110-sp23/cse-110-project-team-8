8da99625912c155f6852f7eaf5781e3643362ab7
huyiscool <47095828+huyiscool@users.noreply.github.com>
2023-05-08 20:10:31 -0700

    Update IntegrationTest.java

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -24,7 +24,7 @@ public class IntegrationTest {
         String prompt = Whisper.transcribe(testAudio);
         System.out.println(prompt);
         String response = ChatGPT.getResponse(prompt, 100); 
-        //assertEquals(prompt, "Who was the tallest man alive?");
-        //assertEquals(response, ""); 
+        assertEquals(prompt, "Who was the tallest man alive?");
+        assertEquals(response, "The tallest man ever recorded was Robert Wadlow, an American man who stood 8 feet 11.1 inches (2.72 meters) tall. He lived from 1918 to 1940."); 
     }
 }
