60c2f34e15d09a31b259e227b9c569379205a296
claireconner <clairemconner@gmail.com>
2023-05-08 18:02:07 -0700

    Update AudioRecorderTest.java

--- test/AudioRecorderTest.java
+++ test/AudioRecorderTest.java
@@ -1,8 +1,8 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
