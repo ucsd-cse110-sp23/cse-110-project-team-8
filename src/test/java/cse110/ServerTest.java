1855abe0cf2f716e90f3e7e59de5c5f0913ade18
huyiscool <47095828+huyiscool@users.noreply.github.com>
2023-05-15 00:57:12 -0700

    Adding tests for put and delete methods of Server class

--- src/test/java/cse110/ServerTest.java
+++ src/test/java/cse110/ServerTest.java
@@ -15,6 +15,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
 import java.util.concurrent.*;
+import java.net.URLEncoder;
 
 public class ServerTest {
     private static final int SERVER_PORT = 8100;
@@ -48,20 +49,34 @@ public class ServerTest {
 
     // @Test
     // public void testGetRequest() {
+    //     // First, post some data
     //     HttpClient client = HttpClient.newHttpClient();
-    //     HttpRequest request = HttpRequest.newBuilder()
-    //             .uri(URI.create("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/?index=0"))
+    //     String json = "{\"prompt\": \"question\", \"response\": \"answer\"}";
+    //     HttpRequest postRequest = HttpRequest.newBuilder()
+    //             .uri(URI.create("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/"))
+    //             .header("Content-Type", "application/json")
+    //             .POST(BodyPublishers.ofString(json, StandardCharsets.UTF_8))
     //             .build();
-    //     HttpResponse<String> response = null;
     //     try {
-    //         response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    //         client.send(postRequest, HttpResponse.BodyHandlers.ofString());
     //     } catch (IOException | InterruptedException e) {
     //         e.printStackTrace();
     //     }
 
-    //     // Assuming that the DataManager is initially empty,
-    //     // the response should indicate that there's no data for the requested index
-    //     assertEquals("No data for this index", response.body());
+    //     // Now, make a get request
+    //     HttpRequest getRequest = HttpRequest.newBuilder()
+    //             .uri(URI.create("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/?index=0"))
+    //             .build();
+    //     HttpResponse<String> getResponse = null;
+    //     try {
+    //         getResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+    //     } catch (IOException | InterruptedException e) {
+    //         e.printStackTrace();
+    //     }
+
+    //     // Assuming the server correctly returns the data we just posted,
+    //     // the response should be that data
+    //     assertEquals("{\"prompt\": \"question\", \"response\": \"answer\"}", getResponse.body());
     // }
 
     @Test
@@ -83,6 +98,47 @@ public class ServerTest {
         // Assert that the response message is as expected
         assertEquals("Posted entry {\"question\", \"answer\"}", response.body());
     }
+
+    @Test
+    public void testPutRequest() {
+        HttpClient client = HttpClient.newHttpClient();
+        String json = "{\"prompt\": \"test question\", \"response\": \"test answer\"}";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/"))
+                .header("Content-Type", "application/json")
+                .PUT(BodyPublishers.ofString(json, StandardCharsets.UTF_8))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Assert that the response code is as expected
+        assertEquals(200, response.statusCode());
+    }
+
+    @Test
+    public void testDeleteRequest() {
+        HttpClient client = HttpClient.newHttpClient();
+        String encodedPathParameter = URLEncoder.encode("test question", StandardCharsets.UTF_8);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/" + encodedPathParameter))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Assert that the response code is as expected
+        assertEquals(200, response.statusCode());
+    }
+
 
 
 }
