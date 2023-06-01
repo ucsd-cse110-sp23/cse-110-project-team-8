f60666439fbcd6bcdf61fd88b267ede75ebf1f2a
yih365 <68409635+gigi365@users.noreply.github.com>
2023-06-01 16:55:15 -0700

    Get email info from database

---
+++ src/main/java/cse110/EmailInfoHandler.java
@@ -0,0 +1,85 @@
+package cse110;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
+public class EmailInfoHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Request Received";
+        String method = httpExchange.getRequestMethod();
+
+        try {
+            if (method.equals("GET")) {
+                response = handleGet(httpExchange);
+            } else if (method.equals("POST")) {
+                response = handlePost(httpExchange);
+            } else {
+                throw new Exception("Not Valid Request Method");
+            }
+        } catch (Exception e) {
+            System.out.println("An erroneous request");
+            response = e.toString();
+            e.printStackTrace();
+        }
+
+        // Sending back response to the client
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream outStream = httpExchange.getResponseBody();
+        outStream.write(response.getBytes());
+        outStream.close();
+    }
+
+    /**
+     * Put email info into database
+     * @param httpExchange
+     * @return 
+     * @throws IOException
+     */
+    private String handlePost(HttpExchange httpExchange) throws IOException {
+        InputStream inStream = httpExchange.getRequestBody();
+        Scanner scanner = new Scanner(inStream);
+        String postData = scanner.nextLine();
+        JsonObject jsonObj = JsonParser.parseString(postData).getAsJsonObject();
+
+        String username = jsonObj.get("username").getAsString();
+        String password = jsonObj.get("password").getAsString();
+        
+        String response = CreateDB.addUser(username, password);
+        System.out.println(response);
+        scanner.close();
+
+        return response;
+    }
+    
+    /**
+     * Get email info from database.
+     * @param httpExchange
+     * @return
+     * @throws IOException
+     */
+    private String handleGet(HttpExchange httpExchange) throws IOException {
+        URI uri = httpExchange.getRequestURI();
+        String query = uri.getRawQuery();
+
+        // Get prompt 
+        String userId = "";
+        if (query != null) {
+            userId = query.substring(query.indexOf("userId=") + "userId=".length());
+        }
+
+        if (!ReadEmailDB.existsEmailInfo(userId)) {
+            // No Email info for this account
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.addProperty("error", "No email info for this account");
+        }
+
+        // Return email info as json object as string
+        return EmailInfo.fromDocument(ReadEmailDB.getEmailInfo(userId)).toJson().toString();
+    }
+}
---
+++ src/main/java/cse110/ReadEmailDB.java
@@ -0,0 +1,39 @@
+package cse110;
+
+
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.*;
+
+
+public class ReadEmailDB extends DBAccess {
+    public static boolean existsEmailInfo(String userId) {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase accountsDB = mongoClient.getDatabase("email");
+            MongoCollection<Document> userCollection = accountsDB.getCollection("info");
+
+            // find one document with Filters.eq()
+            Document emailInfoDoc = userCollection.find(eq(EmailInfo.userIdKey, userId)).first();
+
+            if (emailInfoDoc == null) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public static Document getEmailInfo(String userId) {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase accountsDB = mongoClient.getDatabase("email");
+            MongoCollection<Document> userCollection = accountsDB.getCollection("info");
+
+            // find one document with Filters.eq()
+            Document emailInfoDoc = userCollection.find(eq(EmailInfo.userIdKey, userId)).first();
+            return emailInfoDoc;
+        }
+    }
+}
+
+
