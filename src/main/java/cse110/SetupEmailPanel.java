b81dcc058bc3f2c8fa12620c8979b240f60a24b8
claireconner <clairemconner@gmail.com>
2023-06-03 13:32:04 -0700

    Adding other voice commands
    specific voice commands: empty, invalid command, delete prompt, clear all, and question

--- src/main/java/cse110/SetupEmailPanel.java
+++ src/main/java/cse110/SetupEmailPanel.java
@@ -1,20 +1,8 @@
 package cse110;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.border.Border;
+
 
 public class SetupEmailPanel extends JPanel{
     private JTextArea firstName; 
